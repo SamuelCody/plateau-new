2c230ca8897ab9a95adddb065f456c5ef27e75fb
Samuel Ayinde <ayindesamuel0@gmail.com>
2021-09-20 04:47:47 +0100

    Second commit

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let scrollBtn = document.getElementById("scroll-btn");
 let coreFeatures = document.getElementById("core-features");
 let sendBtn = document.getElementById("send-mail");
 let notifyText = document.getElementById("plateaumed-notify");
+let emailInput = document.getElementById("email");
+let url = "https://dev.plateaumed.com/send-email/";
 document.addEventListener("scroll", () => {
   if (window.scrollY > 150) {
     nav.style.boxShadow = ".1rem .2rem .3rem rgba(0, 0, 0, .08)";
@@ -23,7 +25,29 @@ scrollBtn.addEventListener("click", () => {
   });
 });
 
-sendBtn.addEventListener("click", () => {
+sendBtn.addEventListener("click", async () => {
+  let email = emailInput.value.trim();
+  const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (email === "") {
+    notifyText.style.color = "red";
+    notifyText.innerHTML = "please input an email address";
+    notifyText.style.color = "red";
+  } else if (!email.match(mailformat)) {
+    notifyText.innerHTML = "please input a valid email address";
+    notifyText.style.color = "red";
+  } else {
+    const response = await fetch(`${url}${email}`);
+
+    const result = await response.json();
+    if (!response.ok) {
+      notifyText.innerHTML = "An error occured, please try again";
+      notifyText.style.color = "red";
+    } else {
+      notifyText.innerHTML = result.message;
+      notifyText.style.color = "#02c22c";
+      emailInput.value = "";
+    }
+  }
   notifyText.style.display = "block";
   setTimeout(() => {
     notifyText.style.display = "none";
